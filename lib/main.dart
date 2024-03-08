d759ba0cc10235cec01d9c588bb313e851d0916a
Leonardo Freitas <leonardofreitasdev@gmail.com>
2024-03-07 21:45:15 -0300

    feat: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:vcardy/app.dart';
+
+void main() {
+  runApp(const App());
+}
---
+++ lib/shared/app_images.dart
@@ -0,0 +1,3 @@
+abstract class AppImages {
+  static const String logo = 'assets/images/logo.png';
+}
